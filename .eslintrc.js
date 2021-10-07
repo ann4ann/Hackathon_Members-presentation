3985def953b6b468e41b4086644c5d5b50a6f10e
Anna Noskova <anni4anny@gmail.com>
2021-10-07 21:20:41 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    semi: [2, "always"],
+    "space-before-function-paren": [
+      "error",
+      { anonymous: "always", named: "never" }
+    ],
+    quotes: ["error", "double", { allowTemplateLiterals: true }]
+  }
+};
