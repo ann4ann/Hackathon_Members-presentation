f01511ac40400ad1fdf9dd3a9756ef9cbae1f5f4
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-08 20:25:34 +0300

    feat: add AppRouter and public routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 import NavBar from "./components/Navbar/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <AppRouter />
     </BrowserRouter>
   );
 }
