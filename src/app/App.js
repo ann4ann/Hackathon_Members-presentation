f9c5d8dabd3bd2b576f4ec86b969758c782e9ffe
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-09 16:16:53 +0300

    feat: add particles

--- src/app/App.js
+++ src/app/App.js
@@ -4,9 +4,20 @@ import AppRouter from "./components/AppRouter";
 import Container from "./components/Container";
 import NavBar from "./components/UI/Navbar/NavBar";
 
+import Particles from "react-particles-js";
+import particlesConfig from "./assets/particlesConfig.json";
+
 function App() {
   return (
     <BrowserRouter>
+      <Particles
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0"
+        }}
+        params={particlesConfig}
+      />
       <Container>
         <NavBar />
         <AppRouter />
