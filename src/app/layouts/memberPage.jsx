6445ce498836d4216f70440f69b0e317796c0d93
vladimir konovalov <kvvma@bk.ru>
2021-10-10 14:24:53 +0300

    add breadcrapms

--- src/app/layouts/memberPage.jsx
+++ src/app/layouts/memberPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import AllMembers from "../components/UI/allMembers";
 import members from "../data/members";
+import Breadcramps from "../components/UI/breadcramps";
 
 const MembersPage = () => {
-  return <AllMembers members={members} />;
+  return (
+    <>
+      <Breadcramps />
+      <AllMembers members={members} />
+    </>
+  );
 };
 
 export default MembersPage;
