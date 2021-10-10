b23b16dd78b49c120e541a2e1cb4d2f6e55e5008
semi4ka <semi4ka3@gmail.com>
2021-10-10 18:28:32 +0300

    init progress

---
+++ src/app/components/UI/progress/progress.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./progress.css";
+
+const Progress = ({ percent, name, color, type }) => {
+  if (type === "circle") {
+    let stylesRight;
+    let stylesLeft;
+    if (percent <= 50) {
+      stylesRight = {
+        "border-color": color || "#0d6efd",
+        transform: `rotate(${(percent / 100) * 360}deg)`
+      };
+    } else {
+      stylesRight = {
+        transform: `rotate(180deg)`,
+        "border-color": color || "#0d6efd"
+      };
+      stylesLeft = {
+        transform: `rotate(${((percent - 50) / 100) * 360}deg)`,
+        "border-color": color || "#0d6efd"
+      };
+    }
+
+    return (
+      <>
+        <div className="title">{name}</div>
+
+        <div className="progressC mx-auto">
+          <span className="progress-left">
+            <span className="progress-bar" style={stylesLeft}></span>
+          </span>
+          <span className="progress-right">
+            <span className="progress-bar" style={stylesRight}></span>
+          </span>
+          <div className="progress-value w-100 h-100 rounded-circle d-flex align-items-center justify-content-center">
+            <div className="h2 font-weight-bold">
+              {percent}
+              <sup className="small">%</sup>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="title">{name}</div>
+      <div className="progress">
+        <div
+          className="progress-bar"
+          role="progressbar"
+          style={{ width: percent + "%", background: color || "#0d6efd" }}
+          aria-valuenow={percent}
+          aria-valuemin="0"
+          aria-valuemax="100">
+          {percent}%
+        </div>
+      </div>
+    </>
+  );
+};
+Progress.propTypes = {
+  percent: PropTypes.number.isRequired,
+  name: PropTypes.string,
+  color: PropTypes.string,
+  type: PropTypes.string
+};
+export default Progress;
