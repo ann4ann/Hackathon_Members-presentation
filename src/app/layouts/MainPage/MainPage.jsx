615a80b1f036c7849d9212a54de905abca69cbaf
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-17 01:51:01 +0300

    refactor: add styles for main page

--- src/app/layouts/MainPage/MainPage.jsx
+++ src/app/layouts/MainPage/MainPage.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Breadcramps from "../../components/UI/Breadcramps/Breadcramps";
-import ButtonComponent from "../../components/UI/Button/Button";
+import Button from "../../components/UI/Button/Button";
 import cl from "./main.module.css";
 
 const MainPage = () => {
   return (
     <>
       <Breadcramps />
-      <div>
-        <h1>
-          <p
-            align="center"
-            className="d-flex justify-content-between m-1 bg-dark bg-gradient bg-opacity-50 fs-4">
-            В команду, которая разработала Реакт-приложение, где вы сейчас
-            находитесь, входит 5 человек. Каждый из нас очень любит Реакт,
-            JavaScript и Фронтенд в целом. Мы очень старались, когда
-            разрабатывали это приложение и надеемся оно вам понравится!
-          </p>
-        </h1>
-        <ButtonComponent name="meet">
-          <Link className={cl.main} to="/members">
-            <h1>Meet our team</h1>
-          </Link>
-        </ButtonComponent>
-        <Link className={cl.main} to="/members">
-          <h1>Meet our team</h1>
+      <div className={cl.mainWrapper}>
+        <p align="center" className={cl.mainText}>
+          В команду, которая разработала Реакт-приложение, где вы сейчас
+          находитесь, входит 5 человек. Каждый из нас очень любит Реакт,
+          JavaScript и Фронтенд в целом. Мы очень старались, когда разрабатывали
+          это приложение и надеемся оно вам понравится!
+        </p>
+
+        <Link to="/members">
+          <Button className={cl.mainBtn}>
+            <span>Познакомиться поближе &#128521; </span>
+          </Button>
         </Link>
       </div>
     </>
